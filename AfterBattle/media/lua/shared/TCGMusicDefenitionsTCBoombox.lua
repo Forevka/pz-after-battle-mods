79cf1ac01554259b93c791e50fd0f61ba65a7c21
Forevka <zebestforevka@gmail.com>
2022-01-05 20:43:46 +0200

    update music

--- AfterBattle/media/lua/shared/TCGMusicDefenitionsTCBoombox.lua
+++
@@ -1,6 +0,0 @@
-require "TCMusicDefenitions"
-
-	GlobalMusic["CassetteGrazhdanskayaOboronaGosudarstvo"] = "tsarcraft_music_01_62"
-	GlobalMusic["CassetteGrazhdanskayaoboronaMoyaoborona"] = "tsarcraft_music_01_62"
-	GlobalMusic["CassetteGrazhdanskayaOboronaSistema"] = "tsarcraft_music_01_62"
-
